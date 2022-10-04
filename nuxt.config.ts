5ab3a2f4a0d3e2ebc98e58085568b4c9e25a4654
Dixit <dixxit@protonmail.com>
2022-10-04 23:12:27 +0200

    feat: add favicon and some meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,27 @@
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'CalculaChien',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Divers calculateurs pour chien',
+        },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      ],
+      htmlAttrs: {
+        lang: 'fr',
+      },
+    },
+  },
   css: [
     'vuetify/lib/styles/main.sass',
     '@fortawesome/fontawesome-free/scss/fontawesome.scss',
