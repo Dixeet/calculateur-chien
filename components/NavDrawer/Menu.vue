3482f31d9aed690e7c86c71a8448df0abf151f30
Dixit <dixxit@protonmail.com>
2022-10-04 23:12:57 +0200

    feat: add theme toggle

--- components/NavDrawer/Menu.vue
+++ components/NavDrawer/Menu.vue
@@ -1,64 +1,78 @@
 <script setup>
-const emit = defineEmits(['close']);
+  import { useTheme } from 'vuetify';
+  const emit = defineEmits(['close']);
 
-const navElements = [
-  { text: 'Accueil', icon: 'fa-solid fa-house', to: '/' },
-  { text: 'Calculateurs', type: 'list-subheader' },
-  {
-    text: 'Ration de nourriture',
-    icon: 'fa-solid fa-scale-unbalanced',
-    to: '/calculateurs/ration',
-  },
-  {
-    text: 'Poids idéal',
-    icon: 'fa-solid fa-weight-scale',
-    to: '',
-  },
-  {
-    type: 'list-subheader',
-    text: 'Nourriture',
-  },
-  {
-    text: 'Croquettes',
-    icon: 'fa-solid fa-bowl-rice',
-    to: '',
-  },
-  {
-    text: 'Pâtées',
-    icon: 'fa-solid fa-bowl-food',
-    to: '',
-  },
-];
-function emitCloseEvent() {
-  emit('close');
-}
+  const navElements = [
+    { text: 'Accueil', icon: 'fa-solid fa-house', to: '/' },
+    { text: 'Calculateurs', type: 'list-subheader' },
+    {
+      text: 'Ration de nourriture',
+      icon: 'fa-solid fa-scale-unbalanced',
+      to: '/calculateurs/ration',
+    },
+    {
+      text: 'Poids idéal',
+      icon: 'fa-solid fa-weight-scale',
+      to: '',
+    },
+    {
+      type: 'list-subheader',
+      text: 'Nourriture',
+    },
+    {
+      text: 'Croquettes',
+      icon: 'fa-solid fa-bowl-rice',
+      to: '',
+    },
+    {
+      text: 'Pâtées',
+      icon: 'fa-solid fa-bowl-food',
+      to: '',
+    },
+  ];
+
+  function emitCloseEvent() {
+    emit('close');
+  }
+
+  const theme = useTheme();
+  function toggleTheme() {
+    theme.global.name.value = theme.global.current.value.dark
+      ? 'customLight'
+      : 'customDark';
+  }
 </script>
 <template>
   <div class="d-flex flex-row-reverse pa-1">
     <v-btn
+      aria-label="close menu"
       class="ma-1"
       variant="text"
       icon="fa-solid fa-xmark"
-      @click="emitCloseEvent"
-    />
+      @click="emitCloseEvent" />
+    <v-spacer />
+    <v-btn
+      aria-label="change de theme"
+      class="ma-1"
+      variant="text"
+      icon="fa-solid fa-circle-half-stroke"
+      @click="toggleTheme" />
   </div>
   <v-divider />
   <div>
     <v-list nav density="compact">
       <template
         v-for="({ type, text, icon, to, textClass }, index) in navElements"
-        :key="index"
-      >
+        :key="index">
         <v-list-subheader
           v-if="type === 'list-subheader'"
           :class="textClass ? textClass : 'text-subtitle-2'"
-          >{{ text }}</v-list-subheader
-        >
+          >{{ text }}
+        </v-list-subheader>
 
         <v-divider
           v-else-if="type === 'divider'"
-          :class="textClass ? textClass : 'mt-2'"
-        />
+          :class="textClass ? textClass : 'mt-2'" />
 
         <v-list-item v-else active-color="primary" tag="NuxtLink" :to="to">
           <template v-if="icon" #prepend>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -14,24 +14,37 @@ export default defineNuxtPlugin((nuxtApp) => {
       },
     },
     theme: {
-      defaultTheme: 'custom',
+      defaultTheme: 'customDark',
       variations: {
         colors: ['primary', 'secondary'],
         lighten: 2,
         darken: 2,
       },
       themes: {
-        custom: {
+        customDark: {
           dark: true,
           colors: {
-            background: '#1f1b16',
-            surface: '#1f1b16',
             primary: '#ffb865',
             secondary: '#e0c1a3',
-            error: '#ffb4ab',
-            info: '#9d8e81',
+            background: '#1f1b16',
+            surface: '#1f1b16',
             success: '#bdcd9d',
             warning: '#ffb4ab',
+            error: '#ffb4ab',
+            info: '#9d8e81',
+          },
+        },
+        customLight: {
+          dark: false,
+          colors: {
+            primary: '#7b5800',
+            secondary: '#6c5c3f',
+            background: '#fffbff',
+            surface: '#fffbff',
+            success: '#4c6545',
+            warning: '#ba1a1a',
+            error: '#ba1a1a',
+            info: '#7f7667',
           },
         },
       },
